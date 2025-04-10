269c5b9c470e9818c99c8b918f66c3f65c4f3e54
EdgeCourse <106573186+EdgeCourse@users.noreply.github.com>
2025-04-10 15:19:03 -0500

    Delete ReactVitest directory

--- ReactVitest/starter-files/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-//import { defineConfig as defineVitestConfig } from 'vitest/config';
-
-// Vite configuration with Vitest setup
-export default defineConfig({
-  plugins: [react()],
- 
-});
--- ReactVitest/starter-files/vitest.setup.js
+++
@@ -1,2 +0,0 @@
-import '@testing-library/jest-dom'; // Extends jest with useful matchers for DOM elements (e.g., toBeInTheDocument)
-import { vi } from 'vitest'; // Optional: for mocking global methods
